734901149d21841538b23cdf26d1a755c7e897f9
Bulatsh <bulat768@yandex.ru>
2020-02-15 13:09:45 +0300

    environment variables prod

--- src/app/services/history.js
+++ src/app/services/history.js
@@ -6,12 +6,3 @@ export const activeNavToggle = url => {
     LocalStore.set({ ...state, activeNavUrl: url });
   }
 };
-export const getActiveNavToggle = currentUrl => {
-  const state = LocalStore.get();
-  const { activeNavUrl, redirect = true } = state;
-  if (!redirect && activeNavUrl !== currentUrl) {
-    LocalStore.set({ ...state, redirect: true });
-    return activeNavUrl;
-  }
-  return false;
-};
